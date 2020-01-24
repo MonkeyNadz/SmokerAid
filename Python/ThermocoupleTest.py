78a0eb57487414566033ae05c20fcf739f6c747a
Grant <grant.flynn@btinternet.com>
2020-01-24 15:56:09 +0000

    Comments +Learning outputs

--- Python/ThermocoupleTest.py
+++ Python/ThermocoupleTest.py
@@ -3,12 +3,20 @@ import busio
 import digitalio
 import adafruit_max31856
 
-#SPI Busio Object
+#SPI Bus object (busio based) - just defines the grouping of pins for SPI
+# - adafruit_bus_device/spi_device.py
 SPI = busio.SPI(board.SCK, board.MOSI, board.MISO)
 
+#Chip Select - if we want multiple SPI slave devices - can set multiple CS for
+#    multiple slave SPI chips.
 CS = digitalio.DigitalInOut(board.D5)
+
+#Output as its chip selection - and default high - sets to low to select that
+#  slave chip and then send data out of MOSI
 CS.direction = digitalio.Direction.OUTPUT
 
+#Finally set up thermocouple object - defaults to K Type
 thermocouple = adafruit_max31856.MAX31856(SPI,CS)
 
+# The .temperature reads the register - sets CS to low - and reads
 print(thermocouple.temperature)
