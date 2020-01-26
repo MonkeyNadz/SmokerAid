3bfa3f54e36e4660774d30998cb7e496a8d2f9bb
Grant <grant.flynn@btinternet.com>
2020-01-26 15:38:24 +0000

    TYpos

--- Python/RelayTempTest.py
+++ Python/RelayTempTest.py
@@ -7,7 +7,7 @@ import adafruit_max31856
 def thermocoupleSetup(CSPin):
     SPI = busio.SPI(board.SCK,board.MOSI,board.MISO)
     CS = digitalio.DigitalInOut(CSPin)
-    CS.direction = digitalio.Direction.Output
+    CS.direction = digitalio.Direction.OUTPUT
     thermocouple = adafruit_max31856.MAX31856(SPI,CS)
     return thermocouple
 
@@ -17,12 +17,12 @@ class RelayControl:
 
     def __init__(self,RelayPin):
         self.controlPin = RelayPin
-        self.controlPin.direction = digitalio.Direction.Output
+        self.controlPin.direction = digitalio.Direction.OUTPUT
 
-    def RelayOn:
+    def RelayOn(self):
         self.controlPin.value = True
 
-    def RelayOff:
+    def RelayOff(self):
         self.controlPin.value = False
 
 if __name__ == "__main__":
@@ -31,6 +31,7 @@ if __name__ == "__main__":
 
     while True:
         cur_temp = thermocouple.temperature
+	print(cur_temp)
         if cur_temp >= 24:
             fan.RelayOn()
         else:
